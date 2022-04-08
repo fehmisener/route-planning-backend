671e8e0e6a1bb97e9a5d4cb204422db22caa9b74
Fehmi <fehmiisener@gmail.com>
2022-04-08 23:02:42 +0300

    Delete unnecessary files

--- certain_algorithm.py
+++
@@ -1,8 +0,0 @@
-""""
-1- durak-yolcu listesini azalan sıraya göre listele
-2- ilk araç kapasistesi kadar yolcuyu seçer en yukarıdan,
-3- yolcu kalmayan duraklar listeden çıkarılır
-3- 2. ve 3. adım tüm araçlar için tekrar eder
-
-
-"""
--- helper_methods.py
+++
@@ -1,5 +0,0 @@
-def passenger_counter(passenger_list):
-    total_passenger_count = 0
-    for the_key, the_value in passenger_list.items():
-        total_passenger_count = total_passenger_count + the_value
-    return total_passenger_count
