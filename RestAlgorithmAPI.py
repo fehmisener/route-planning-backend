12b21ba6c35129567fa1a56a78ac145d916aec5e
Fehmi <fehmiisener@gmail.com>
2022-04-14 17:31:57 +0300

    test

--- RestAlgorithmAPI.py
+++ RestAlgorithmAPI.py
@@ -41,31 +41,6 @@ def get_routes_for_limited_car():
         "route_list": routes_list,
     }, 200
 
-
-@algorithm_api.route("/algorithm/limited-car/route-save", methods=["POST"])
-def save_routes_for_limited_car():
-
-    route_date = request.json["route_date"]
-    route_list = main(route_date)
-
-    try:
-        for i in route_list:
-            counter = 0
-            for j in route_list[i]:
-                cur.execute(
-                    "insert into route values (?, ?,?, ?)",
-                    (i.split("_")[1], j["station_id"], counter, route_date),
-                )
-                con.commit()
-                counter = counter + 1
-        return route_list, 200
-    except:
-        return {
-            "msg": 'You cannot calculate routes twice in one day. You can press the "Show Routes" button to see the route you calculated before.',
-            "status_code": 400,
-        }, 400
-
-
 @algorithm_api.route("/algorithm/limited-car/route", methods=["POST"])
 def get_route_for_user():
 
@@ -105,7 +80,6 @@ def get_route_for_user():
         return {"msg": "No route for this user in table.", "status_code": 403}, 403
     return {"service_route": query_result, "user_id": user_id}, 200
 
-
 @algorithm_api.route("/algorithm/clear", methods=["DELETE"])
 def clear_table():
 
@@ -113,4 +87,29 @@ def clear_table():
     cur.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='route'")
     con.commit()
 
-    return {"Table cleared." "status_code": 200}, 200
+    return {"msg":"Table cleared.", "status_code": 200}, 200
+
+@algorithm_api.route("/algorithm/limited-car/route-save", methods=["POST"])
+def save_routes_for_limited_car():
+
+    route_date = request.json["route_date"]
+    route_list = main(route_date)
+
+    try:
+        for i in route_list:
+            counter = 0
+            for j in route_list[i]:
+                cur.execute(
+                    "insert into route values (?, ?, ?, ?)",
+                    (i.split("_")[1], j["station_id"], counter, route_date),
+                )
+                con.commit()
+                counter = counter + 1
+        return route_list, 200
+    except:
+        return {
+            "msg": 'You cannot calculate routes twice in one day. You can press the "Show Routes" button to see the route you calculated before.',
+            "status_code": 400,
+        }, 400
+
+#TODO RETURN SPESIFIC ROUTE
